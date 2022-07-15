03bd236714668e25b156c569239e50f3b3f0e0fc
radmilovict <radmilovic70@gmail.com>
2022-07-16 01:49:38 +0200

    kreiran navigacioni bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Footer from './komponente/Footer';
+import NavBar from './komponente/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar></NavBar>
       <Footer></Footer>
     </div>
   );
